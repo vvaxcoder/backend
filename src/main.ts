ac4c6b504077f3783881e4394813b8a5ec5dd6dc
VVoronkov <voronglass84@gmail.com>
2025-03-02 17:46:08 +0300

    Use simple env variables throw ConfigModule. @//TODO create config folder

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('APP_PORT');
+  await app.listen(PORT ?? 3000);
 }
 
 bootstrap();
