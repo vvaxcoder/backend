8afcdde0d1fe734c85483e952ac9a64f48e9e9a5
VVoronkov <voronglass84@gmail.com>
2025-03-05 15:05:47 +0300

    Create .sequelizerc file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.model';
+import { TestModule } from './test/test.module';
 
 console.log('DB_USER:', process.env.DB_USER);
 
@@ -29,6 +30,7 @@ console.log('DB_USER:', process.env.DB_USER);
       }),
     }),
     UsersModule,
+    TestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
