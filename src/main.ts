7fe9640681257f2bee89e9d223a8e6e3e8aa7f0b
VVoronkov <voronglass84@gmail.com>
2025-03-18 17:40:37 +0300

    Add prefix api

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('APP_PORT');
+  app.setGlobalPrefix('api');
   await app.listen(PORT ?? 3000);
 }
 
