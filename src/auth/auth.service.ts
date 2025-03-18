1e0d19465d0ee4597c13d84e6187982f20db9a94
VVoronkov <voronglass84@gmail.com>
2025-03-18 17:34:41 +0300

    Remove bcrypt, add bycryptjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
-import * as bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/users/users.model';
 
